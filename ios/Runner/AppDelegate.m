6cc5def01a067a44a4e207910a62c8348971fba7
fluttertraining <50535228+fluttertraining@users.noreply.github.com>
2019-10-06 10:22:04 +0800

    Update AppDelegate.m

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,13 +1,11 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
-#import "GoogleMaps/GoogleMaps.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
-  [GMSServices provideAPIKey:@"AIzaSyBzCry4GX0kvDynifV9mC2AdffYLY79nks"];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
